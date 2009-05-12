048e77c466bd329b91e08848cf0076fbceb3296d
rene moser <mail@renemoser.net>
2009-05-12 18:43:57 +0200

    typo, fixes #1

--- src/main/java/net/renemoser/libjsms/App.java
+++ src/main/java/net/renemoser/libjsms/App.java
@@ -8,21 +8,24 @@ import net.renemoser.libjsms.service.*;
 public class App {
 	public static void main(String[] args) {
 		if (args.length < 4) {
-			System.err.println("Use: java -cp net.renemoser.libjsms.App <userid> <password> <phoneNumber> <message>");
+			System.err.println("Use: java -cp LibJSMS-x.y.jar net.renemoser.libjsms.App <userid> <password> <phoneNumber> <message>");
 			System.exit(0);
 		}
 		
-		String userid = args[0]+"";
-		String password = args[1]+"";
-		String phoneNumber = args[2]+"";
-		String message = args[3]+"";
+		String userid = args[0] + "";
+		String password = args[1] + "";
+		String phoneNumber = args[2] + "";
+		String message = args[3] + "";
 				
 	    try {    	
 	    	ShortMessageService Sunrise = new Sunrise();
     	    Sunrise.doLogin(userid, password);
     	    Sunrise.sendShortMessage(phoneNumber, message);
     	    if (!Sunrise.getShortMessage().isEmpty()) {
-    	    	System.out.println("Message '" + Sunrise.getShortMessage() + "' was sent to " + Sunrise.getPhoneNumber());
+    	    	System.out.println("Message '" + 
+    	    			Sunrise.getShortMessage() + 
+    	    			"' was sent to " + 
+    	    			Sunrise.getPhoneNumber());
     	    }
     	} catch(Exception e) {
     	    e.printStackTrace();
